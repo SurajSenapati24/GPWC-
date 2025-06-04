5ede5d656dc5807b0778b711c4685d6734ab366e
Suraj Senapati <surajsenapati58@gmail.com>
2025-06-04 23:55:09 +0530

    update

--- MinorAssignment6/Question_03.cpp
+++ MinorAssignment6/Question_03.cpp
@@ -4,10 +4,8 @@ using namespace sf;
 int main() {
     IntRect rect(100, 100, 400, 300);
     View viewFromRect(FloatRect(rect.left, rect.top, rect.width, rect.height));
-
     Vector2f center(300.f, 250.f);
     Vector2f size(400.f, 300.f);
     View viewFromCenter(center, size);
-
     return 0;
 }
